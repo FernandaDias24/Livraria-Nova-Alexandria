ba2059b831899d094232131bf72ad140374944ae
alana-as <abs_alana@hotmail.com>
2022-08-23 09:38:32 -0300

    novos elementos

--- index.js
+++ index.js
@@ -2,3 +2,17 @@ import { Express } from "express";
 import LivrariaController from "./Controller/LivrariaController.js";
 import LivrariaModels from "./Models/LivrariaModels.js";
 
+class AcervoDeLivros
+{
+    const catalogoLivros(idLivro, Titulo, Autor, Editora, Genero, ISBN, Quantidade)
+    {
+        this.idLivro = idLivro
+        this.Titulo = Titulo
+        this.Autor = Autor
+        this.Editora = Editora
+        this.Genero = Genero
+        this.ISBN = ISBN
+        this.Quantidade = Quantidade
+    }
+
+}
